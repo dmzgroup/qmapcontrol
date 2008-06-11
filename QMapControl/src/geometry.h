6c6235c085a1a25f69963dfbebc7226be63f6035
kaiwinter <kaiwinter@7a14e55a-7340-0410-9c5e-d514c3f7b88f>
2008-06-11 09:15:33 +0000

    gpl licence information added
    git-svn-id: https://qmapcontrol.svn.sourceforge.net/svnroot/qmapcontrol@10 7a14e55a-7340-0410-9c5e-d514c3f7b88f

--- QMapControl/src/geometry.h
+++ QMapControl/src/geometry.h
@@ -1,22 +1,28 @@
-/***************************************************************************
- *   Copyright (C) 2007 by Kai Winter   *
- *   kaiwinter@gmx.de   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+/*
+*
+* This file is part of QMapControl,
+* an open-source cross-platform map widget
+*
+* Copyright (C) 2007 - 2008 Kai Winter
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with QMapControl. If not, see <http://www.gnu.org/licenses/>.
+*
+* Contact e-mail: kaiwinter@gmx.de
+* Program URL   : http://qmapcontrol.sourceforge.net/
+*
+*/
+
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 #include <QObject>
